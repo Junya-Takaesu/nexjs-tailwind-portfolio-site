8e3fba7f1e00a827b4458414b799dfbf03b26a09
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2022-09-03 23:28:07 +0900

    Finish About Section

---
+++ components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex items-center w-full p-2 py-16 md:h-screen">
+      <div className="max-w-[1040px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">About</p>
+          <h2 className="py-4">Who I Am</h2>
+          <p className="py-2 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur natus voluptate fugiat aut aspernatur facilis labore. Unde nesciunt qui quasi,
+            aperiam laboriosam dolor explicabo veniam, quia voluptates, id ex fugit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur natus
+            voluptate fugiat aut aspernatur facilis labore. Unde nesciunt qui quasi, aperiam laboriosam dolor explicabo veniam, quia voluptates, id ex
+            fugit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur natus voluptate fugiat aut aspernatur facilis labore. Unde nesciunt qui
+            quasi, aperiam laboriosam dolor explicabo veniam, quia voluptates, id ex fugit.
+          </p>
+          <p className="py-2 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus qui blanditiis accusamus exercitationem aliquid consectetur sed et provident
+            deleniti rem. Eveniet perspiciatis vero error quae deserunt, in consequatur cum iure.
+          </p>
+        </div>
+        <div className="flex items-center justify-center w-full h-auto p-4 m-auto duration-100 ease-in shadow-xl shadow-gray-400 rounded-xl hover:scale-105 ">
+          <img
+            className="rounded-xl"
+            src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2564&q=80"
+            alt="/"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/Main.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+function Main() {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="text-sm tracking-widest text-gray-600">なにか一緒につくろ〜</p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I&lsquo;m <span className="text-[#5651e5]">順や</span>
+          </h1>
+          <h1 className="py-4 text-gray-700">よわよわデベロッパー</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            特に何かに長けたわけでもないしがないよわよわデベロッパー。バグの生産と風紀を乱すことが特技のジャーク。
+            最近のところなにかしているわけでもなく、地球の酸素の割合をへらすことに尽力しているし、下水管に安定的に人糞を供給することに定評がある。
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="w-16 h-16 p-6 duration-100 ease-out rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110">
+              <FaLinkedin />
+            </div>
+            <div className="w-16 h-16 p-6 duration-100 ease-out rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110 ">
+              <FaGithub />
+            </div>
+            <div className="w-16 h-16 p-6 duration-100 ease-out rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110 ">
+              <AiOutlineMail />
+            </div>
+            <div className="w-16 h-16 p-6 duration-100 ease-out rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110 ">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
